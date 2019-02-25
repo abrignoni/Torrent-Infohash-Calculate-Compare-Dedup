1a734a9058e1c30f29790ff47e8cf63cb7c8cc51
Brigs <abrignoni@gmail.com>
2019-02-25 12:47:10 -0500

    Add files via upload

---
+++ infohashCC.py
@@ -0,0 +1,102 @@
+import argparse, sys
+import os.path 
+from os import path
+import glob
+import bencoding, hashlib
+
+parser = argparse.ArgumentParser(description='Calculate and/or compare .torrent infohash values')
+parser.add_argument("-o",choices=['calculate', 'compare'], required=True, 
+                    help="Calculate or compare infohashes")
+parser.add_argument("-p", "--path", required=True, help="Path to .torrent files. For compare include list.txt in same directory as the script")
+
+if len(sys.argv[1:])==0:
+    parser.print_help()
+    # parser.print_usage() # for just the usage line
+    parser.exit()
+	
+args = parser.parse_args()
+
+script_dir = os.path.dirname(__file__)
+caldict = dict()
+calfail = dict()
+totals = 0
+
+print ('')
+print ('Infohash calculate & compare')
+print ('By: @AlexisBrignoni')
+print ('Web: abrignoni.com')
+print ('')
+
+
+
+def infohash(fname):
+	objTorrentFile = open(fname, "rb")
+	decodedDict = bencoding.bdecode(objTorrentFile.read())
+	info_hash = hashlib.sha1(bencoding.bencode(decodedDict[b"info"])).hexdigest()
+	return info_hash
+
+#print(args)
+print('Selected: ' + args.o)
+print('Search directory: '+ args.path) 
+#print(args.path)
+
+if args.path is not None:
+	pathcheck = os.path.exists(args.path) #verify directory exists
+	if pathcheck:
+		#If there are .torrent files in it
+		#print('Path is real: ' +args.path)
+		for filename in glob.iglob(args.path+'\**', recursive=True):
+			if os.path.isfile(filename):
+				if filename.lower().endswith(('.torrent')):
+					try:
+						calhash = infohash(filename) #Place values in dictionary
+						caldict[calhash] = filename
+					except:
+						#print(filename+' is not a torrent file') #open text file to save failed values of calc. 
+						calhash = 0
+						calfail[calhash] = filename
+						continue
+	else: #Path does not exists
+		print('Path is not valid. \n')
+		parser.print_help()
+		parser.exit()	
+		
+if args.o == 'calculate':
+	if len(caldict) > 0:
+		print('Calculated infohashes: ' + str(len(caldict)))
+		print('')
+		z=open('./calculated.txt', 'w+')
+		for key, value in caldict.items():
+			print(key+' => '+value)
+			name = os.path.basename(value)
+			z.write(key + '\t' + name + '\t' + value + '\n')
+			#print the array, mention the text file.
+		z.close()
+		print('')
+		print('See calculated.txt')
+	else:
+		print('No torrents to process')
+else:
+	try:
+		with open(script_dir+ 'list.txt') as f:
+			lines = f.read().splitlines()#Insert txt values from list.txt in array. If zero values error. if values compare both arrays. Similar in a txt list.
+		if len(lines) > 0:
+			print('Infohashes to compare: '+ str(len(lines)))
+			print('')
+			w=open('./matches.txt', 'w+')
+			for x in range(len(lines)):
+				comparator = lines[x]
+				if comparator in caldict.keys():
+					print(comparator+' => '+caldict[comparator])
+					names = os.path.basename(caldict[comparator])
+					w.write(str(comparator) + '\t' + names + '\t' + str(caldict[comparator]) + '\n')
+					totals = totals+1
+			w.close()
+			print('')
+			print('Matching infohases total: '+ str(totals))
+			if totals > 0:
+				print('See matches.txt')
+		else:
+			print('No matching infohashes. Verify list.txt')
+	except:
+		print('')
